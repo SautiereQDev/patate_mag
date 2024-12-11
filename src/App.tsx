56218625dd708f0767896b764d0b00520c64bdf4
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-11 11:51:32 +0100

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import HomePage from "./components/pages/HomePage.tsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
-
-		return (
-			<>
-					<HomePage/>
-			</>
-		)
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/article/:id" element={<ArticlePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
