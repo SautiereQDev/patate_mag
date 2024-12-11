d69d9f85d2e339da1af0d81d88621eb5a4ce2088
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-11 11:52:03 +0100

    HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { articles } from '../data/articles';
+import ArticleCard from '../components/ArticleCard';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {articles.map(article => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
