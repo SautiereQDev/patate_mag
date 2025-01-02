4b1d46ea8f8d6f9dd6a740226a083f073993cd2a
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2025-01-02 12:39:07 +0100

    use cors on backend

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import { connectDB } from './database';
 import articleRoutes from './routes/articles';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
-// Middleware
+const corsOptions = {
+  origin: 'http://localhost:5173', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors(corsOptions));
 
 app.get('/', articleRoutes);
 
