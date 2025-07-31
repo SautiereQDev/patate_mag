5f80177d2e3ffd7e9c9089ef4cf0171a99ba605c
SautiereQDev <contact@quentinsautiere.com>
2025-07-31 14:38:52 +0200

    update route paths for articles and images to include prefix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,16 @@
 import express, {Application, Request, Response} from 'express';
 import articleRoutes from './routes/articlesRoutes';
 import imageRoutes from './routes/imagesRoutes';
-import cors from 'cors';
 
 export const app: Application = express();
 
-const corsOptions = {
-	origin:
-		process.env.NODE_ENV === 'development'
-			? `http://localhost:${process.env.PORT}`
-			: 'https://quentinsautiere.com',
-	methods: 'GET,HEAD,PUT,POST,DELETE',
-	credentials: true, // corrigé de widthCredentials
-	optionsSuccessStatus: 204
-};
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors(corsOptions));
 
 // Routes
-app.use('/articles', articleRoutes);
-app.use('/images', imageRoutes);
+app.use('/patate-mag/articles', articleRoutes);
+app.use('/patate-mag/images', imageRoutes);
 
 app.use((err: Error, req: Request, res: Response) => {
 	console.error('Erreur : ', err.message);
